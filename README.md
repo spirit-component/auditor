c6a6be95ce8df45fb8e58e449c6ef6e6153d8d32
Zeal <xujinzheng@gmail.com>
2018-11-24 23:46:29 +0800

    first available version

---
+++ README.md
@@ -0,0 +1,141 @@
+Auditor
+=======
+
+
+`build.conf`
+
+
+```
+packages = ["github.com/spirit-component/auditor"]
+```
+> add packages to build.conf
+
+
+#### config
+
+
+```
+components.auditor.name-of-this-actor {
+
+	caches {
+			audit {
+			driver = "go-cache"
+			options = {
+				expiration = 30s
+				cleanup-interval = 5m
+			}
+		}
+	}
+
+
+	report {
+		interval = 1s
+		log-size= 10
+		body-limit = 10
+	}
+
+	sls {
+		endpoint = "cn-beijing.log.aliyuncs.com"
+        access-key-id = ""
+        access-key-secret = ""
+
+        project = gogap
+        store = test 
+
+        topic = auditor
+        source = gogap
+	}
+}
+```
+
+
+### graph
+
+```
+entrypoint {
+	to-audit-begin {
+		seq = 1
+		url = "spirit://actors/fbp/auditor/todo-auditor?action=begin"
+	}
+
+	to-todo {
+		seq = 2
+		url = "spirit://actors/fbp/examples-todo/todo?action=new"
+	}
+
+	to-audit-end {
+		seq = 3
+		url = "spirit://actors/fbp/auditor/todo-auditor?action=end&checkpoint=this-is-optional"
+	}
+
+	response {
+		seq = 4
+		url = "spirit://actors/fbp/postapi/external?action=callback"
+	}
+}
+```
+> limition: to-audit-begin and to-audit-end should be in same component instance
+
+content in aliyun sls looks like:
+
+```
+__source__:  gogap
+__tag__:__client_ip__:  x.x.x.x
+__tag__:__receive_time__:  1543073242
+__topic__:  auditor
+begin_body:  {
+"name"......
+begin_from:  
+end_body:  {"id":"55f......
+end_from:  spirit://actors/fbp/examples-todo/todo?action=new
+header:  {"X-Api":"todo.task.new","content-type":"application/json"}
+id:  e4aecf98-c884-4ef5-8dec-45e8c382ed2f
+mode:  paired
+time_costs:  0.0007590000s
+timestamp:  2018-11-24 15:27:22.091059 +0000 UTC
+```
+
+
+or 
+
+
+```
+entrypoint {
+	
+	to-audit {
+		seq = 1
+		url = "spirit://actors/fbp/auditor/todo-auditor?action=step&checkpoint=before-create-task"
+	}
+
+	to-todo {
+		seq = 2
+		url = "spirit://actors/fbp/examples-todo/todo?action=new"
+	}
+
+	response {
+		seq = 3
+		url = "spirit://actors/fbp/postapi/external?action=callback"
+	}
+}
+```
+> no limition, because the audit log is not ranged
+
+content in aliyun sls looks like:
+
+```
+__source__:  gogap
+__tag__:__client_ip__:  x.x.x.x
+__tag__:__receive_time__:  1543073229
+__topic__:  auditor
+body:  {
+"name":""
+}
+checkpoint:  error
+error:  task name is empty
+from:  spirit://actors/fbp/examples-todo/todo?action=new
+header:  {"X-Api":"todo.task.new","content-type":"application/json"}
+id:  5ce050e3-24fc-4370-936e-66e6533a1024
+mode:  single
+time_costs:  0.0001950000s
+timestamp:  2018-11-24 15:27:08.639661 +0000 UTC
+```
---
+++ auditor.go
@@ -0,0 +1,419 @@
+package auditor
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/go-spirit/spirit/cache"
+	"github.com/go-spirit/spirit/component"
+	"github.com/go-spirit/spirit/mail"
+	"github.com/go-spirit/spirit/worker"
+
+	"github.com/aliyun/aliyun-log-go-sdk"
+	"github.com/gogo/protobuf/proto"
+	"github.com/sirupsen/logrus"
+
+	"github.com/spirit-component/auditor/internal/queue/goring"
+)
+
+var (
+	goringInitSize int64 = 64
+)
+
+type Audit struct {
+	opts component.Options
+
+	alias string
+
+	aesKey []byte
+
+	cache cache.Cache
+
+	logStore  *sls.LogStore
+	queue     *goring.Queue
+	logSize   int64
+	bodyLimit int
+
+	topic  string
+	source string
+
+	interval time.Duration
+
+	stopChan    chan struct{}
+	stoppedChan chan struct{}
+}
+
+func init() {
+	component.RegisterComponent("auditor", NewAudit)
+}
+
+func NewAudit(alias string, opts ...component.Option) (comp component.Component, err error) {
+
+	auditOpts := component.Options{}
+
+	for _, o := range opts {
+		o(&auditOpts)
+	}
+
+	return &Audit{
+		alias: alias,
+		opts:  auditOpts,
+	}, nil
+}
+
+func (p *Audit) Start() (err error) {
+
+	slsEndPoint := p.opts.Config.GetString("sls.endpoint")
+
+	if len(slsEndPoint) == 0 {
+		err = fmt.Errorf("sls.endpoint is empty")
+		return
+	}
+
+	slsAccessKeyId := p.opts.Config.GetString("sls.access-key-id")
+
+	if len(slsAccessKeyId) == 0 {
+		err = fmt.Errorf("sls.access-key-id is empty")
+		return
+	}
+
+	slsAccessKeySecret := p.opts.Config.GetString("sls.access-key-secret")
+
+	if len(slsAccessKeySecret) == 0 {
+		err = fmt.Errorf("sls.access-key-secret is empty")
+		return
+	}
+
+	slsProject := p.opts.Config.GetString("sls.project")
+
+	if len(slsProject) == 0 {
+		err = fmt.Errorf("sls.project is empty")
+		return
+	}
+
+	slsStore := p.opts.Config.GetString("sls.store")
+
+	if len(slsStore) == 0 {
+		err = fmt.Errorf("sls.store is empty")
+		return
+	}
+
+	slsTopic := p.opts.Config.GetString("sls.topic")
+	slsSource := p.opts.Config.GetString("sls.source")
+
+	logInterval := p.opts.Config.GetTimeDuration("report.interval", time.Second)
+
+	logProj, err := sls.NewLogProject(slsProject, slsEndPoint, slsAccessKeyId, slsAccessKeySecret)
+	if err != nil {
+		return
+	}
+
+	logStore, err := sls.NewLogStore(slsStore, logProj)
+	if err != nil {
+		return
+	}
+
+	auditCache, exist := p.opts.Caches.Require("audit")
+	if !exist {
+		err = fmt.Errorf("the cache of 'audit' not exist")
+		return
+	}
+
+	p.logStore = logStore
+	p.cache = auditCache
+	p.logSize = p.opts.Config.GetInt64("report.log-size", 10)
+	p.bodyLimit = int(p.opts.Config.GetInt64("report.body-limit", 0))
+	p.queue = goring.New(goringInitSize)
+	p.topic = slsTopic
+	p.source = slsSource
+	p.interval = logInterval
+	p.stopChan = make(chan struct{})
+	p.stoppedChan = make(chan struct{})
+
+	go p.postWorker()
+
+	return nil
+}
+
+func (p *Audit) Stop() error {
+
+	p.stopChan <- struct{}{}
+	<-p.stoppedChan
+
+	return nil
+}
+
+func (p *Audit) Alias() string {
+	if p == nil {
+		return ""
+	}
+	return p.alias
+}
+
+type AuditContent struct {
+	Id        string
+	Header    map[string]string
+	BeginBody []byte
+	EndBody   []byte
+	Err       error
+	BeginTime time.Time
+	EndTime   time.Time
+	BeginFrom string
+	EndFrom   string
+}
+
+func (p *Audit) Begin(session mail.Session) (err error) {
+
+	id := session.Payload().ID()
+	header := session.Payload().Content().GetHeader()
+
+	var body = copyBytes(session.Payload().Content().GetBody())
+
+	if p.bodyLimit > 0 && len(body) > p.bodyLimit {
+		body = append(body[:p.bodyLimit], []byte("......")...)
+	}
+
+	sErr := session.Err()
+
+	p.cache.Set(id, &AuditContent{
+		Id:        id,
+		Header:    header,
+		BeginBody: body,
+		Err:       sErr,
+		BeginTime: time.Now().UTC(),
+		BeginFrom: session.From(),
+	},
+	)
+
+	return
+}
+
+func (p *Audit) End(session mail.Session) (err error) {
+
+	id := session.Payload().ID()
+	sErr := session.Err()
+
+	icontent, exist := p.cache.Get(id)
+
+	if !exist {
+		return
+	}
+
+	content, ok := icontent.(*AuditContent)
+
+	if !ok {
+		return
+	}
+
+	if content == nil {
+		return
+	}
+
+	if sErr != nil {
+		content.Err = sErr // update response err
+	}
+
+	strHeader := ""
+
+	if len(content.Header) > 0 {
+		data, _ := json.Marshal(content.Header)
+		strHeader = string(data)
+	}
+
+	content.EndTime = time.Now().UTC()
+	content.EndFrom = session.From()
+
+	var body = copyBytes(session.Payload().Content().GetBody())
+
+	if p.bodyLimit > 0 && len(body) > p.bodyLimit {
+		body = append(body[:p.bodyLimit], []byte("......")...)
+	}
+
+	content.EndBody = body
+
+	slsLog := &sls.Log{
+		Time: proto.Uint32(uint32(content.BeginTime.Unix())),
+		Contents: []*sls.LogContent{
+			&sls.LogContent{
+				Key:   proto.String("id"),
+				Value: proto.String(content.Id),
+			},
+			&sls.LogContent{
+				Key:   proto.String("header"),
+				Value: proto.String(string(strHeader)),
+			},
+			&sls.LogContent{
+				Key:   proto.String("timestamp"),
+				Value: proto.String(content.BeginTime.String()),
+			},
+			&sls.LogContent{
+				Key:   proto.String("time_costs"),
+				Value: proto.String(fmt.Sprintf("%0.10fs", content.EndTime.Sub(content.BeginTime).Seconds())),
+			},
+		},
+	}
+
+	if content.Err != nil {
+		slsLog.Contents = append(slsLog.Contents,
+			&sls.LogContent{
+				Key:   proto.String("error"),
+				Value: proto.String(content.Err.Error()),
+			})
+	}
+
+	ckPoint := session.Query("checkpoint")
+	if len(ckPoint) > 0 {
+		slsLog.Contents = append(slsLog.Contents,
+			&sls.LogContent{
+				Key:   proto.String("checkpoint"),
+				Value: proto.String(ckPoint),
+			},
+		)
+	}
+
+	if session.Query("action") != "step" {
+		slsLog.Contents = append(slsLog.Contents,
+			&sls.LogContent{
+				Key:   proto.String("begin_from"),
+				Value: proto.String(content.BeginFrom),
+			},
+			&sls.LogContent{
+				Key:   proto.String("end_from"),
+				Value: proto.String(content.EndFrom),
+			},
+			&sls.LogContent{
+				Key:   proto.String("begin_body"),
+				Value: proto.String(string(content.BeginBody)),
+			},
+			&sls.LogContent{
+				Key:   proto.String("end_body"),
+				Value: proto.String(string(content.EndBody)),
+			},
+			&sls.LogContent{
+				Key:   proto.String("mode"),
+				Value: proto.String("paired"),
+			},
+		)
+	} else {
+		slsLog.Contents = append(slsLog.Contents,
+			&sls.LogContent{
+				Key:   proto.String("from"),
+				Value: proto.String(content.BeginFrom),
+			},
+			&sls.LogContent{
+				Key:   proto.String("body"),
+				Value: proto.String(string(content.BeginBody)),
+			},
+			&sls.LogContent{
+				Key:   proto.String("mode"),
+				Value: proto.String("single"),
+			},
+		)
+	}
+
+	p.queue.Push(slsLog)
+
+	return
+}
+
+func copyBytes(data []byte) []byte {
+	var newBytes = make([]byte, len(data), cap(data))
+	copy(newBytes, data)
+	return newBytes
+}
+
+func (p *Audit) Step(session mail.Session) (err error) {
+	p.Begin(session)
+	p.End(session)
+
+	return
+}
+
+func (p *Audit) postWorker() {
+
+	tick := time.Tick(p.interval)
+
+	for {
+		select {
+		case <-tick:
+			{
+				ilogs, ok := p.queue.PopMany(p.logSize)
+
+				if !ok {
+					continue
+				}
+
+				lg := p.toLogGroup(ilogs)
+
+				if lg == nil {
+					continue
+				}
+
+				err := p.logStore.PutLogs(lg)
+
+				if err != nil {
+					logrus.WithError(err).Errorln("autdit logs failure")
+				}
+			}
+
+		case <-p.stopChan:
+			{
+				if p.queue.Length() > 0 {
+					go func() {
+						time.Sleep(time.Second)
+						p.stopChan <- struct{}{}
+					}()
+					continue
+				}
+
+				p.stoppedChan <- struct{}{}
+				return
+			}
+		}
+	}
+}
+
+func (p *Audit) toLogGroup(logs []interface{}) *sls.LogGroup {
+	if len(logs) == 0 {
+		return nil
+	}
+
+	logGroup := &sls.LogGroup{
+		Source: proto.String(p.source),
+		Topic:  proto.String(p.topic),
+	}
+
+	for _, v := range logs {
+		l, ok := v.(*sls.Log)
+		if ok {
+			logGroup.Logs = append(logGroup.Logs, l)
+		}
+	}
+
+	if len(logGroup.Logs) > 0 {
+		return logGroup
+	}
+
+	return nil
+}
+
+func (p *Audit) Route(session mail.Session) worker.HandlerFunc {
+
+	switch session.Query("action") {
+	case "begin":
+		{
+			return p.Begin
+		}
+	case "end":
+		{
+			return p.End
+		}
+	case "step":
+		{
+			return p.Step
+		}
+	}
+
+	return nil
+}
